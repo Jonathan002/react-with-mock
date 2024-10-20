90182251a1a6d823a2eeea1526c94a9099b3f0e7
Jonathan Fernandez <jonathanf002@gmail.com>
2024-10-19 18:50:25 -0700

    added treeshaking example test

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import FatStr from "./fat";
 
+// Adding Fat Code will increase size by 7MB - (You can check network tab and see: 'main.[hash].js' size)
+// This is to test that tree-shaking of if() statements works
+// Comment out any if statement and check the network tabs after doing a build with either environment
 if (process.env.REACT_APP_ENV === "development") {
   const isDevStrTest = "This is a test string in development";
-  alert(isDevStrTest);
+  alert(isDevStrTest + FatStr);
 }
-
-console.log("====> zz");
+if (process.env.REACT_APP_ENV === "production") {
+  const isDevStrTest = "This is a test string in production";
+  alert(isDevStrTest + FatStr);
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
